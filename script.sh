874f99dcf164912887a186ffa7cd3bb85b16f00b
dsddr02 <fldz5555a@outlook.com>
2025-01-01 14:37:03 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,9 +5,9 @@ cd openwrt
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
 # mv -f package-temp/luci-app-adguardhome package/lean/
 # rm -rf package-temp
-git clone https://github.com/kenzok8/small-package.git theme-temp/small-package
- mv -f theme-temp/small-package package/lean/
-rm -rf theme-temp
+#git clone https://github.com/kenzok8/small-package.git theme-temp/small-package
+# mv -f theme-temp/small-package package/lean/
+#rm -rf theme-temp
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 # Add luci-theme-opentomcat
