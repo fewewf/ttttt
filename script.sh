6928352244e6bb8b52aa615e685e8a584c8461ff
fewewf <32592150+fewewf@users.noreply.github.com>
2025-01-01 21:58:45 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,11 +5,11 @@ cd openwrt
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
 # mv -f package-temp/luci-app-adguardhome package/lean/
 # rm -rf package-temp
-git clone https://github.com/kenzok8/small-package.git theme-temp/small-package
-mv -f theme-temp/small-package package/lean/
-rm -rf theme-temp
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+#git clone https://github.com/kenzok8/small-package.git theme-temp/small-package
+#mv -f theme-temp/small-package package/lean/
+#rm -rf theme-temp
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 # Add luci-theme-opentomcat
 default_theme='Argon'
 # sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
@@ -29,40 +29,3 @@ rm -rf package-temp
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,luci-app-dogcom,transmission,natflow,wireless-regdb}
-rm -rf package/lean/small-package/luci-app-dogcom
-rm -rf package/lean/small-package/transmission
-rm -rf package/lean/small-package/haproxy
-rm -rf package/lean/small-package/natflow
-rm -rf package/lean/small-package/dockerd
-rm -rf package/lean/small-package/docker
-rm -rf feeds/luci/applications/luci-app-filetransfer
-rm -rf package/lean/small-package/luci-app-openvpn-server
-rm -rf package/lean/small-package/openvpn-easy-rsa-whisky
-rm -rf package/lean/small-package/luci-app-wrtbwmon
-rm -rf package/lean/small-package/wrtbwmon
-rm -rf package/lean/small-package/luci-app-koolproxy
-rm -rf package/lean/small-package/luci-app-godproxy
-#rm -rf package/lean/small-package/luci-app-argon*
-#rm -rf package/lean/small-package/luci-theme-argon*
-#rm -rf package/lean/small-package/luci-app-amlogic
-rm -rf package/lean/small-package/luci-app-unblockneteasemusic
-rm -rf package/lean/small-package/upx-static
-rm -rf package/lean/small-package/upx
-rm -rf package/passwall-packages/shadowsocksr*
-rm -rf package/lean/small-package/shadowsocksr*
-rm -rf package/lean/small-package/opkg
-rm -rf package/lean/small-package/base-files
-rm -rf package/lean/small-package/luci-app-bandwidthd
-rm -rf package/lean/small-package/luci-app-bypass
-rm -rf package/lean/small-package/luci-app-dogcom
-rm -rf package/lean/small-package/luci-app-filetransfer
-rm -rf package/lean/small-package/luci-app-nginx-pingos
-rm -rf package/lean/small-package/luci-app-ssr-plus
-rm -rf package/lean/small-package/luci-app-onliner
-rm -rf package/lean/small-package/natflow
-# rm -rf package/lean/small-package/luci-lib-taskd
-
-rm -rf package/feeds/packages/aliyundrive-webdav
-rm -rf feeds/packages/multimedia/aliyundrive-webdav
-rm -rf package/feeds/packages/perl-xml-parser
-rm -rf package/feeds/packages/xfsprogs
