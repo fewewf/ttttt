48f6d855708e2eae4a5d652aa5514f918ec7f77a
fewewf <32592150+fewewf@users.noreply.github.com>
2025-01-02 08:57:17 +0800

    Update script.shy

--- script.sh
+++ script.sh
@@ -8,8 +8,8 @@ cd openwrt
 #git clone https://github.com/kenzok8/small-package.git theme-temp/small-package
 #mv -f theme-temp/small-package package/lean/
 #rm -rf theme-temp
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+sed -i '$a src-git ampackage https://github.com/kenzok8/small-package' feeds.conf.default
+#rm -rf feeds/ampackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 # Add luci-theme-opentomcat
 default_theme='Argon'
 # sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
@@ -25,21 +25,7 @@ git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-a
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small-package.git package/small-package
+#git clone https://github.com/kenzok8/small-package.git package/small-package
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,luci-app-dogcom,transmission,natflow,wireless-regdb}
-rm -rf package/small-package/luci-app-bandwidthd
-rm -rf package/small-package/luci-app-bypass
-rm -rf package/small-package/luci-app-dogcom
-rm -rf package/small-package/luci-app-gowebdav
-rm -rf package/small-package/luci-app-nginx-pingos
-rm -rf package/small-package/luci-app-ssr-plus
-rm -rf package/small-package/luci-app-onliner
-rm -rf package/small-package/natflow
-rm -rf package/small-package/haproxy
-rm -rf package/small-package/transmission
-rm -rf package/feeds/packages/aliyundrive-webdav
-rm -rf feeds/packages/multimedia/aliyundrive-webdav
-rm -rf package/feeds/packages/perl-xml-parser
-rm -rf package/feeds/packages/xfsprogs
