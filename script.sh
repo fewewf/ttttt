814e3908394d3c03f5db09baad06ce4c156688df
fewewf <32592150+fewewf@users.noreply.github.com>
2025-01-02 11:50:24 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,4 +29,4 @@ git clone https://github.com/kenzok8/small-package.git package/small-package
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,luci-app-dogcom,transmission,natflow,wireless-regdb}
-rm -rf package/small-package/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,natflow,luci-app-bandwidthd,luci-app-bypass,luci-app-dogcom}
+rm -rf package/small-package/{base-files,haproxy,transmission,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,natflow,luci-app-bandwidthd,luci-app-bypass,luci-app-dogcom}
