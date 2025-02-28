d3c4767b071dad0fb871cee6169221478aeab4b5
dsddr02 <fldz5555a@outlook.com>
2025-02-28 19:41:29 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,17 +15,17 @@ default_theme='Argon'
 # sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i 's/192.168.1.1/192.168.110.247/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.110.247/g' package/base-files/luci2/bin/config_generate
-sed -i 's/192.168.1.1/192.168.110.247/g' package/base-files/Makefile
-sed -i 's/192.168.1.1/192.168.110.247/g' package/base-files/image-config.in
+sed -i 's/192.168.1.1/192.168.1.80/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.80/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.80/g' package/base-files/Makefile
+sed -i 's/192.168.1.1/192.168.1.80/g' package/base-files/image-config.in
 
 # Add luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/kenzok8/small-package package/small-package
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 rm -rf package/small-package/luci-app-openvpn-server
